f033a85765e5c82cbfe984ed00a3641b2069f5cf
rRichboy <tvinktvink893@gmail.com>
2023-10-05 18:33:31 +0300

    Добавьте файлы проекта.

---
+++ Avto/Avto.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avto
+{
+    class Avto
+    {
+        private string nomerAvto;
+        private double kolichestvoBenzina;
+        private double rashodTopliva;
+        private double ostatokTopliva;
+        private double skorost;
+        private double probeg;
+
+        public void Info(string nom, double bak, double ras)
+        {
+            nomerAvto = nom;
+            kolichestvoBenzina = bak;
+            rashodTopliva = ras;
+            ostatokTopliva = bak;
+            skorost = 0.0;
+            probeg = 0.0;
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"Номер авто: {nomerAvto}");
+            Console.WriteLine($"Количество бензина в баке: {ostatokTopliva} л");
+            Console.WriteLine($"Расход топлива на 100 км: {rashodTopliva} л/100км");
+            Console.WriteLine($"Текущая скорость: {skorost} км/ч");
+            Console.WriteLine($"Ваш пробег: {probeg} км");
+        }
+
+        public void Zapravka(double top)
+        {
+            kolichestvoBenzina += top;
+            ostatokTopliva += top;
+        }
+
+        public void Move(int speed, int distance)
+        {
+            if (speed <= 0)
+            {
+                Console.WriteLine("Скорость должна быть больше нуля.");
+                return;
+            }
+
+
+
+            double rashodToplivaNaKm = rashodTopliva / 100;
+            double rashod = distance * rashodToplivaNaKm;
+
+            if (ostatokTopliva >= rashod)
+            {
+                ostatokTopliva -= rashod;
+                probeg += distance;
+                Console.WriteLine($"Проехано {distance} км со скоростью {speed} км/ч. Остаток топлива: {ostatokTopliva:F2} л");
+                skorost = speed;
+            }
+
+            else
+
+            {
+                int maxDistance = (int)(ostatokTopliva / rashodToplivaNaKm);
+
+                if (maxDistance > 0)
+                {
+                    Console.WriteLine($"Недостаточно топлива для поездки на всю дистанцию {distance} км.");
+                    Console.WriteLine($"Машина проедет {maxDistance} км со скоростью {speed} км/ч.");
+                    probeg += maxDistance;
+                    Console.WriteLine("Машина проехала максимальное расстояние с текущим количеством топлива.");
+
+                    Console.Write("Желаете дозаправить машину? (Да/Нет): ");
+                    string choice = Console.ReadLine();
+                    if (choice.ToLower() == "да")
+                    {
+                        Console.Write("Введите количество топлива для дозаправки: ");
+                        double topUpAmount = double.Parse(Console.ReadLine());
+                        Zapravka(topUpAmount);
+                        Console.WriteLine($"Машина дозаправлена на {topUpAmount:F2} л.");
+                        Move(speed, distance - maxDistance);
+                    }
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Недостаточно топлива для поездки.");
+                }
+            }
+        }
+
+        private double Ostatok()
+        {
+            return ostatokTopliva;
+        }
+
+        public void Tormozhenie()
+        {
+            Console.WriteLine("Автомобиль замедляется.");
+            skorost = 0.0;
+        }
+
+        public void Razgon(int additionalSpeed)
+        {
+            if (ostatokTopliva >= 1.0)
+            {
+                skorost += additionalSpeed;
+                rashodTopliva += 0.5;
+                ostatokTopliva -= 1.0;
+                Console.WriteLine($"Автомобиль разгоняется до скорости {skorost} км/ч. Расход топлива увеличен.");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно топлива для разгона.");
+            }
+        }
+
+        private void proydennoerast(int distance)
+        {
+            this.probeg += distance;
+        }
+    }
+}
---
+++ Avto/Main.cs
@@ -0,0 +1,72 @@
+﻿namespace avto
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Avto avto1 = new Avto();
+
+            Console.Write("Введите номер авто: ");
+            string nomer = Console.ReadLine();
+
+            Console.Write("Введите количество бензина в баке (в литрах): ");
+            double bak = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите расход топлива на 100 км (в л/100км): ");
+            double ras = double.Parse(Console.ReadLine());
+
+            avto1.Info(nomer, bak, ras);
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1 - Поехать");
+                Console.WriteLine("2 - Разогнать автомобиль");
+                Console.WriteLine("3 - Тормозить");
+                Console.WriteLine("4 - Заправить автомобиль");
+                Console.WriteLine("5 - Вывести информацию");
+                Console.WriteLine("6 - Выход");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Введите скорость для поездки (в км/ч): ");
+                        int speed = int.Parse(Console.ReadLine());
+                        Console.Write("Введите расстояние: ");
+                        int distance = int.Parse(Console.ReadLine());
+                        avto1.Move(speed, distance);
+                        break;
+
+                    case 2:
+                        Console.Write("Введите скорость для разгона (в км/ч): ");
+                        int additionalSpeed = int.Parse(Console.ReadLine());
+                        avto1.Razgon(additionalSpeed);
+                        break;
+
+                    case 3:
+                        avto1.Tormozhenie();
+                        break;
+
+                    case 4:
+                        Console.Write("Введите количество бензина для заправки (в литрах): ");
+                        double top = double.Parse(Console.ReadLine());
+                        avto1.Zapravka(top);
+                        break;
+
+                    case 5:
+                        avto1.Out();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Программа завершена.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный выбор. Повторите попытку.");
+                        break;
+                }
+            }
+        }
+    }
+}
